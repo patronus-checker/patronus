32cc89ec5ae5f17eec27b949e2ee07af5a0efe67
Jan Tojnar <jtojnar@gmail.com>
2018-05-23 16:23:48 +0200

    bindings: restructure C bindings build system

--- bindings/c/src/lib.rs
+++ bindings/c/src/lib.rs
@@ -1,26 +1,30 @@
 extern crate patronus;
 extern crate patronus_provider;
 
-use patronus_provider::{Annotation, AnnotationArray, Properties, Suggestion};
+pub use patronus_provider::{Annotation, AnnotationArray, Properties, Suggestion};
 use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
 
+// TODO: cbindgen needs to support opaque structs
+// https://github.com/eqrion/cbindgen/issues/104
+// or respect visibility
+// https://github.com/eqrion/cbindgen/issues/123
 /// Opaque wrapper for `Patronus` struct.
-#[repr(C)]
-pub struct Patronus(patronus::Patronus);
+#[no_mangle]
+pub enum Patronus {}
 
 /// Creates an instance of `Patronus` checker.
 /// The returned value should be cleaned using `patronus_free` after use.
 #[no_mangle]
 pub extern "C" fn patronus_create() -> *mut Patronus {
-    Box::into_raw(Box::new(Patronus(patronus::Patronus::new())))
+    Box::into_raw(Box::new(patronus::Patronus::new())) as *mut Patronus
 }
 
 /// Cleans up the `Patronus` object returned by `patronus_create`.
 #[no_mangle]
 pub unsafe extern "C" fn patronus_free(ptr: *mut Patronus) {
     assert!(!ptr.is_null(), "Trying to free a NULL pointer.");
-    Box::from_raw(ptr);
+    Box::from_raw(ptr as *mut patronus::Patronus);
 }
 
 /// Checks provided text for mistakes.
@@ -34,7 +38,7 @@ pub unsafe extern "C" fn patronus_check(
     assert!(!ptr.is_null(), "Trying to use a NULL pointer.");
     assert!(!props.is_null(), "Trying to use a NULL pointer.");
 
-    let patronus = &(*ptr).0;
+    let patronus = &(*(ptr as *mut patronus::Patronus));
 
     let properties = {
         let Properties { primary_language } = *props;
